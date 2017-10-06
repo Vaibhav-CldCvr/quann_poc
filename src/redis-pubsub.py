8c52bae1d3bbbfd9d328b8ac827d54b8b01bb381
paritosh16 <paritosh.walvekar@gmail.com>
2017-10-06 17:42:18 +0530

    pubsub/dataflow

--- src/redis-pubsub.py
+++ src/redis-pubsub.py
@@ -1,5 +1,11 @@
 import redis
 import time
+#import argparse
+
+#import apache_beam as beam
+#from apache_beam.options.pipeline_options import PipelineOptions
+#from apache_beam.options.pipeline_options import GoogleCloudOptions
+#from apache_beam.options.pipeline_options import StandardOptions
 
 from google.cloud import pubsub_v1
 
@@ -35,8 +41,29 @@ def receive_messages(project, subscription_name):
     print('Listening for messages on {}'.format(subscription_path))
     while True:
         time.sleep(60)
+       
 
+"""Data Streaming not supported in Python SDK. Use Java SDK"""
 
+"""def read_from_pubsub(argv=None):
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--input_topic', dest='input_topic', required=True,
+        help='Input PubSub topic of the form "/topics/<PROJECT>/<TOPIC>".')
+    parser.add_argument(
+        '--output_topic', dest='output_topic', required=True,
+        help='Output PubSub topic of the form "/topics/<PROJECT>/<TOPIC>".')
+    known_args, pipeline_args = parser.parse_known_args(argv)
+    
+    with beam.Pipeline(argv=pipeline_args) as p:
+        lines = p | beam.io.ReadStringsFromPubSub(known_args.input_topic)
+        # Capitalize the characters in each line.
+        transformed = (lines
+                    | 'capitalize' >> (beam.Map(lambda x: x.upper())))
+
+        # Write to PubSub.
+        # pylint: disable=expression-not-assigned
+        transformed | beam.io.WriteStringsToPubSub(known_args.output_topic)"""
 
 if __name__ == '__main__':
     project = 'cloudcover-sandbox'
@@ -45,3 +72,4 @@ if __name__ == '__main__':
     r = redis.StrictRedis(host='localhost', port=6379, db=0)
     publish_messages(project, topic_name,r)
     receive_messages(project,subscription_name)
+    
